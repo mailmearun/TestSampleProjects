9dda44039de3a607931eee6787d0fa6ef948165e
mailme.arun60@gmail.com <mailme.arun60@gmail.com>
2019-01-21 17:07:25 +0530

    TestOPT

---
+++ TestCucumberOPT/src/main/java/com/test/cucumber/stepDefinitions/TestStepDefinition.java
@@ -0,0 +1,24 @@
+package com.test.cucumber.stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TestStepDefinition {
+
+    @Given("^Launch Google\\.com$")
+    public void launch_Google_com() throws Throwable {
+        System.out.println("Step 1");
+    }
+
+    @When("^Enter valid Search keyword$")
+    public void enter_valid_Search_keyword() throws Throwable {
+        System.out.println("Step 2");
+    }
+
+    @Then("^Search Displayed$")
+    public void search_Displayed() throws Throwable {
+        System.out.println("Step 3");
+    }
+
+}
